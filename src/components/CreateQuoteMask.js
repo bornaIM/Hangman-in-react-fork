48e5e0b23045ec0f2671c7f5fe2bc2500378a70a
Borna Bezjak <borna.bezjak@ingemark.com>
2022-12-10 16:05:34 +0100

    review, added first batch of TODO comments

--- src/components/CreateQuoteMask.js
+++ src/components/CreateQuoteMask.js
@@ -1,8 +1,14 @@
+// TODO: popravit formating koda
+// TODO: ovo je funkcija, a ne React komponenta. Dakle trebalo bi napravit dvije stvari: 
+// 1.) ime funkcije treba bit napisano malim pocetnim slovom
+// 2.) ovo treba bit u drugom folderu, a ne u components. Components folder je za React componente, a ovo je obicna funkcija
 import IsALetter from "./IsALetter"
 export default function CreateQuoteMask(quote, visableLetters){
     let mask = ""
     for (let char of quote) {
-        if (!visableLetters.includes(char.toUpperCase()) && !visableLetters.includes(char.toLowerCase())){
+        // TODO: ja bih osobno ovo razdvojio u 2 retka, malo je lakse za citat, ali to je opcionalno :)
+        if (!visableLetters.includes(char.toUpperCase()) && !visableLetters.includes(char.toLowerCase())) {
+            //TODO:
             if(IsALetter(char)){
                 mask += "_"
             } else if(char == " "){
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from 'axios'
+// TODO: dosta vam je da style ukljucite u root komponentu, ne treba vam u svakoj komponenti
 import "../style"
 
 export default function Scoreboard(){
     let gameData = JSON.parse(localStorage.getItem("gameData"))
     const [scoreList, setScoreList] = React.useState([{"errors": gameData.errors,"userName": gameData.userName, "id": -1}])
 
-
+    // TODO: inace za ovakve stvari mozete iskoristit object destructuring, npr. umjesto
+    // import React from 'react' napisete
+    // import { useEffect } from 'react', dodete na isto a izgleda puno ljepse :D
     React.useEffect(() => {
         axios.get("https://my-json-server.typicode.com/stanko-ingemark/hang_the_wise_man_frontend_task/highscores").then((response) => {
             setScoreList((oldScoreList) => [...oldScoreList, ...response.data])
@@ -20,6 +23,7 @@ export default function Scoreboard(){
         <div>
             <h1>Socreboard</h1>
             <ol className='scoreboard'>
+            { /* TODO: indentacija */ }
             {scoreList.length > 1 && scoreListItems}
             </ol>
         </div>
